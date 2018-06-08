b0f4bfef0d355a9dccc96a09939044c8892031e6
ahmed35m <muhammadmanzoor16@gmail.com>
2018-06-08 01:15:47 -0600

    Added shortestDist and longDist function
    calculates shortest and longest dist with heversine function

--- src/ca/uregina/ahmed35m/Coordinates.java
+++ src/ca/uregina/ahmed35m/Coordinates.java
@@ -1,22 +1,25 @@
 package ca.uregina.ahmed35m;
+import java.util.ArrayList;
 import java.util.Random;
 
 
 public class Coordinates {
 	private double lat;
 	private double longt;
-	
+	public static int earth_radius = 6371;
+	public static int earth_circum = 40075;
+
 	
 	public Coordinates()
 	{
-		lat= 0.0000000;
-		longt=0.000000;
+		this.setLat();
+		this.setLongt();
 	}
 	
 	public double getLat() {
 		return lat;
 	}
-	public void setLat(double lat) {
+	public void setLat() {
 
 		Random rg = new Random();
 		this.lat = rg.nextInt(180)-90+ (rg.nextInt(99999999))*(Math.pow(10, -8));
@@ -25,34 +28,67 @@ public class Coordinates {
 	public double getLongt() {
 		return longt;
 	}
-	public void setLongt(double longt) {
+	public void setLongt() {
 		Random rg = new Random();
 		this.longt = rg.nextInt(360)-180+ (rg.nextInt(99999999))*(Math.pow(10, -8));
 	}
 	
 	
 	public static void main(String[] args) {
-	double a = 0;
-	Random rg = new Random();
+	
 
-	a =rg.nextInt(180)-90+ (rg.nextInt(99999999))*(Math.pow(10, -8));
-	System.out.println(a);
+	System.out.println("Random Location Calculator\n");
 	
 	
+	Coordinates x1 = new Coordinates();
+	Coordinates x2 = new Coordinates();
+	Coordinates x3 = new Coordinates();
+	Coordinates x4 = new Coordinates();
+	Coordinates x5 = new Coordinates();
+	
+	ArrayList<Coordinates> loc = new ArrayList<Coordinates>();
+	loc.add(x1);
+	loc.add(x2);
+	loc.add(x3);
+	loc.add(x4);
+	loc.add(x5);
+	
+	for(int i =0; i<5;i++)
+	{
+		loc.get(i).printCoord();
 	}
 	
-	public double getShortestDist(Coordinates a, Coordinates b)
+	System.out.println("Shortest Distance: " + getShortestDist(x4,x5));
+	System.out.println("Longest Distance: " + getLongDist(x4,x5));
+
+	
+	}
+	public double haversine(double d)
+	{
+		return d/earth_radius;
+	}
+	public static double getShortestDist(Coordinates a, Coordinates b)
 	{
 		double d =0;
+		double dlong = Math.toRadians(a.getLongt() - b.getLongt());
+		double dlat = Math.toRadians(a.getLat() - b.getLat());
 		
+		// Haversine formula implements
+		 double r = Math.pow(Math.sin(dlat / 2),2) + Math.pow(Math.sin(dlong / 2),2) * Math.cos(Math.toRadians(a.getLat())) * Math.cos(b.getLat());
+	     double c = 2 * Math.asin(Math.sqrt(r));
+	     d =r*c;
 		
+	     return d;
+	}
+	public static double getLongDist(Coordinates a, Coordinates b)
+	{
+		double d =0;
+		d = earth_circum - getShortestDist(a,b);
 		return d;
 	}
-	public double getLongDist(Coordinates a, Coordinates b)
+	
+	public void printCoord()
 	{
-		double d =0;
-		
-		
-		return d;
+		System.out.println("Location:" + getLat()+ " ,"+ getLongt());
 	}
 }
